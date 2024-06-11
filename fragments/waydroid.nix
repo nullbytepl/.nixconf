1c8780923f682d5ccea30e6a3b948fe6490c886a
Kamila Wojciechowska <nullbytepl@gmail.com>
2024-06-11 19:51:36 +0200

    Drop useless waydroid service

--- fragments/waydroid.nix
+++ fragments/waydroid.nix
@@ -1,17 +1,4 @@
 { pkgs,... }:
 {
   virtualisation.waydroid.enable = true;
-
-  # Service to start a Waydroid session on boot
-  systemd.services.waydroid = {
-    description = "Waydroid";
-    wantedBy = [ "graphical-session.target" ];
-
-    partOf = [ "graphical-session.target" ];
-
-    serviceConfig = {
-      ExecStart = "${pkgs.waydroid}/bin/waydroid session start";
-      ExecStop = "${pkgs.waydroid}/bin/waydroid session stop";
-    };
-  };
 }
