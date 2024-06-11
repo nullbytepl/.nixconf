f8fce7f0b221f7c5fcc8246dfb46fdc0df8ef822
Kamila Wojciechowska <nullbytepl@gmail.com>
2024-06-11 19:52:05 +0200

    Connect to the keyboard as soon as sddm starts
    Reduces the wait for the keyboard to connect when logging in

--- machine/pc.nix
+++ machine/pc.nix
@@ -11,4 +11,8 @@
 
     # Disable PCIe ASPM as it causes sleep issues
     boot.kernelParams = [ "pcie_aspm=off" ];
+
+    # Connect to the Magic Keyboard as soon as possible (i.e. schedule the connection before sddm loads)
+    # Don't block the boot process ('&')
+    services.displayManager.sddm.setupScript = "${pkgs.bluez}/bin/bluetoothctl connect 48:E1:5C:C3:A5:04 &";
 }
