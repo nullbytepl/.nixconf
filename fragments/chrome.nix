700f0b42dbb4df917f9420898f8515085938fa28
Kamila Wojciechowska <nullbytepl@gmail.com>
2024-10-13 00:53:29 +0200

    Shut down chrome gracefully before powering off
    Gets rid of "Chrome didn't shut down properly"

--- fragments/chrome.nix
+++ fragments/chrome.nix
@@ -8,4 +8,25 @@
         ];
       })
     ];
+
+    # Service to shut down chrome "gracefully" (so it doesn't show the "Chrome didn't shut down properly" message)
+    # https://askubuntu.com/a/1469353
+    systemd.services.murder_chrome = {
+        serviceConfig = {
+          # The killall in pkgs.toybox doesn't support `--wait`, so use pkgs.psmisc
+          ExecStart = ''
+            ${pkgs.psmisc}/bin/killall chrome --wait
+          '';
+          Type = "oneshot";
+          User = "root";
+          Group = "root";
+          Restart = "no";
+        };
+        unitConfig = {
+          DefaultDependencies = "no";
+        };
+        before = [ "shutdown.target" ];
+        wantedBy = [ "halt.target" "reboot.target" "shutdown.target" ];
+        enable = true;
+    };
 }
