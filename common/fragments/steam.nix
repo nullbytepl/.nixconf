5441e08f8982351352dfa432c84d1b510e919bd5
Mila Wojciechowska <nullbytepl@gmail.com>
2025-09-30 16:32:11 +0200

    steam: add proton GE, enable NTSYNC

--- common/fragments/steam.nix
+++ common/fragments/steam.nix
@@ -5,5 +5,20 @@
         enable = true;
         remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
         dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+
+        extraCompatPackages = [
+            pkgs.proton-ge-bin
+        ];
     };
+
+    # NTSYNC support for proton
+    boot.kernelModules = ["ntsync"];
+
+    services.udev.packages = [
+        (pkgs.writeTextFile {
+            name = "ntsync-udev-rules";
+            text = ''KERNEL=="ntsync", MODE="0660", TAG+="uaccess"'';
+            destination = "/etc/udev/rules.d/70-ntsync.rules";
+        })
+    ];
 }
