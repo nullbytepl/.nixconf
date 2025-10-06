b2e3601f018c22afaa1e4f481826e10818b7bdd0
Mila Wojciechowska <nullbytepl@gmail.com>
2025-10-06 23:20:36 +0200

    hw: nvidia driver -> 580.95.05

--- common/fragments/kernel.nix
+++ common/fragments/kernel.nix
@@ -62,7 +62,13 @@ in {
   boot.kernelPackages = linuxPackages_custom;
 
   hardware.nvidia = lib.mkIf (usesNv) {
-    package = config.boot.kernelPackages.nvidiaPackages.latest.overrideAttrs (old: {
+    package = (config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    version = "580.95.05";
+    sha256_64bit = "sha256-hJ7w746EK5gGss3p8RwTA9VPGpp2lGfk5dlhsv4Rgqc=";
+    openSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
+    settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
+    persistencedSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
+  }).overrideAttrs (old: {
       passthru = old.passthru // {
         open = old.passthru.open.overrideAttrs (o: {
           makeFlags = (o.makeFlags or []) ++ [
--- hw/nvidia.nix
+++ hw/nvidia.nix
@@ -20,7 +20,13 @@
     open = false;
     nvidiaSettings = true;
 
-    package = config.boot.kernelPackages.nvidiaPackages.latest;
+    package = (config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "580.95.05";
+      sha256_64bit = "sha256-hJ7w746EK5gGss3p8RwTA9VPGpp2lGfk5dlhsv4Rgqc=";
+      openSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
+      settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
+      persistencedSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
+    });
 
     prime = {
       intelBusId = "PCI:1:0:0";
