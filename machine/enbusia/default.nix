0d698f41f1520b770e5b1602b709c727182cff97
Mila Wojciechowska <nullbytepl@gmail.com>
2025-10-21 18:34:59 +0200

    enbusia: remove redundant includes

--- machine/enbusia/default.nix
+++ machine/enbusia/default.nix
@@ -10,12 +10,9 @@
   imports = lib.flatten [
     ./hardware-configuration.nix
     ./es83xx/audio.nix
-    inputs.nixhardware.nixosModules.common-hidpi
     inputs.nixhardware.nixosModules.common-cpu-amd
     inputs.nixhardware.nixosModules.common-cpu-amd-pstate
     inputs.nixhardware.nixosModules.common-cpu-amd-zenpower
-    inputs.nixhardware.nixosModules.common-pc-laptop-ssd
-    inputs.nixhardware.nixosModules.common-pc-laptop
   ];
 
   networking.hostName = "enbusia";
